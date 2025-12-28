dd5dc87a691b61a9270bc834b4a10363d2fe2bfe
BurakYaba <brkyaba@gmail.com>
2025-12-28 12:59:20 +0300

    made changes to robot.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,22 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: [
-          "/_next/", // Block Next.js build files
-          "/api/", // Block API routes if any
-        ],
-      },
-      {
-        userAgent: "MJ12bot",
-        disallow: "/",
-      },
-      {
-        userAgent: "AhrefsBot",
-        disallow: "/",
-      },
-      {
-        userAgent: "SemrushBot",
-        disallow: "/",
+        // No disallow rules - allow everything
       },
     ],
     sitemap: "https://www.webcraft.tr/sitemap.xml",
